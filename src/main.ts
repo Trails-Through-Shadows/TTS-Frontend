25ffc80b1e644b154cf72be90404f33ce759bf18
Orfian <Orfian963@gmail.com>
2024-04-25 14:45:16 +0200

    Will you work pls?

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,3 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "bootstrap-icons/font/bootstrap-icons.min.css";
-import "simplebar/dist/simplebar.min.css";
-import "simplebar/dist/simplebar.min.js";
-import "jquery/dist/jquery.min.js";
-
 import './main.css'
 import App from './pages/App.svelte'
 
